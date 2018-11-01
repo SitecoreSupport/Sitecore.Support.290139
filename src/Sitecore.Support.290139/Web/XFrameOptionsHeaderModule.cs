2f71edf4b991de64823b3530a3eda78b9f40c059
AndreyFilchenkov <AndreyFilchenkov@users.noreply.github.com>
2018-11-01 17:14:47 +0200

    Fix 290139

--- src/Sitecore.Support.290139/Web/XFrameOptionsHeaderModule.cs
+++ src/Sitecore.Support.290139/Web/XFrameOptionsHeaderModule.cs
@@ -12,7 +12,16 @@
     /// <returns><c>true</c> ifapplication request url is under protected with header directory; otherwise - <c>false</c>.</returns>
     protected override bool IsSitecoreFolderRequest(HttpContextBase httpContext)
     {
-      return httpContext.Request.PhysicalPath.StartsWith(httpContext.Server.MapPath("~/Sitecore/"), StringComparison.InvariantCultureIgnoreCase);
+      try
+      {
+        return httpContext.Request.PhysicalPath.StartsWith(httpContext.Server.MapPath("~/Sitecore/"), StringComparison.InvariantCultureIgnoreCase);
+      }
+      catch (Exception ex)
+      {
+        Sitecore.Diagnostics.Log.Debug("[XFrameOptionsHeaderModule] " + ex.Message + System.Environment.NewLine + ex.StackTrace);
+
+        return true; // In case we cannot identify if the protected folder is requested the data should be protected by the XFrameOptionsHeader
+      }
     }
   }
 }
