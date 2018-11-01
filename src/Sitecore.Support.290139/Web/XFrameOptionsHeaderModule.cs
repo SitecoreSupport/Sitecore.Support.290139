921cf7ec7f6466025d3fc27a935b09a2dadd4cc8
AndreyFilchenkov <AndreyFilchenkov@users.noreply.github.com>
2018-11-01 15:26:42 +0200

    Initial commit + Import the source code

---
+++ src/Sitecore.Support.290139/Web/XFrameOptionsHeaderModule.cs
@@ -0,0 +1,18 @@
+﻿namespace Sitecore.Support.Web
+{
+  using System;
+  using System.Web;
+
+  public class XFrameOptionsHeaderModule : Sitecore.Web.XFrameOptionsHeaderModule
+  {
+    /// <summary>
+    /// Check whether current requested URL is starts with path to sitecore folder.
+    /// </summary>
+    /// <param name="httpContext">Instance of <see cref="HttpContextBase"/>.</param>
+    /// <returns><c>true</c> ifapplication request url is under protected with header directory; otherwise - <c>false</c>.</returns>
+    protected override bool IsSitecoreFolderRequest(HttpContextBase httpContext)
+    {
+      return httpContext.Request.PhysicalPath.StartsWith(httpContext.Server.MapPath("~/Sitecore/"), StringComparison.InvariantCultureIgnoreCase);
+    }
+  }
+}
